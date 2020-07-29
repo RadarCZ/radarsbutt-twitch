96b7a640aad2e7659afb6df4191493f8d69fbfbe
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2020-07-29 15:09:16 +0200

    remove unnecessary commands from available commands

--- src/shared/commands.ts
+++ src/shared/commands.ts
@@ -1,19 +1,15 @@
 import { ITwitchCommand } from './types/CommandTypes';
 import { DiscordCommand } from './types/commands/DiscordCommand';
 import { MmrCommand } from './types/commands/MmrCommand';
-import { PlaysoundCommand } from './types/commands/PlaysoundCommand';
 import { CommandsCommand } from './types/commands/CommandsCommand';
 import { PlaysoundsCommand } from './types/commands/PlaysoundsCommand';
 import { TtsAvailableReminderCommand } from './types/commands/TtsAvailableReminderCommand';
-import { VoicesCommand } from './types/commands/VoicesCommand';
 
 export const TwitchCommands: ITwitchCommand[] = [
 	new DiscordCommand(),
 	new MmrCommand(),
-	new PlaysoundCommand(),
 	new CommandsCommand(),
 	new PlaysoundsCommand(),
-	new VoicesCommand(),
 ];
 
 export const RemiderCommands: ITwitchCommand[] = [
